96077f50213c8e034accab2589456bfcdad7c702
Aad Versteden <madnificent@gmail.com>
2020-03-10 18:52:19 +0100

    Remove unused aliases
    These aliases are not used in the component.  They are generated as a
    base starting point but may be removed in cases like this.

--- addon/components/editor-plugins/dbpedia-info-card.js
+++ addon/components/editor-plugins/dbpedia-info-card.js
@@ -12,29 +12,6 @@ import layout from '../../templates/components/editor-plugins/dbpedia-info-card'
 export default Component.extend({
   layout,
 
-  /**
-   * Region on which the card applies
-   * @property location
-   * @type [number,number]
-   * @private
-  */
-  location: reads('info.location'),
-
-  /**
-   * Unique identifier of the event in the hints registry
-   * @property hrId
-   * @type Object
-   * @private
-  */
-  hrId: reads('info.hrId'),
-
-  /**
-   * The RDFa editor instance
-   * @property editor
-   * @type RdfaEditor
-   * @private
-  */
-  editor: reads('info.editor'),
   description: '',
   image: '',
 
